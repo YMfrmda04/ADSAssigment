8d489534e90e3dca3fe9563c8731b4a2b8db51f1
M Ahmed <mo.ahmed0114@gmail.com>
2024-01-04 03:25:30 +0000

    Add SortingAlgorithm folder attempt 3

---
+++ Project4Sorting/Project4Sorting.Test/UnitTest1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Project4;
+
+namespace Project4.Test;
+
+[TestClass]
+public class AlgorithmsUnitTest
+{
+    SortingAlgorithm algorithm = new SortingAlgorithm();
+
+    [TestMethod]
+    public void BubbleSort_Working_Correctly()
+    {
+        int[] testArray = { 4, 2, 5, 1, 3 };
+        int[] expected = { 1, 2, 3, 4, 5 };
+        algorithm.BubbleSort(testArray);
+        CollectionAssert.AreEqual(expected, testArray,
+            "Is Bubble Sort working as inteneded?");
+    }
+
+    [TestMethod]
+    public void QuickSort_Working_Correctly()
+    {
+        int[] testArray = { 4, 2, 5, 1, 3 };
+        int[] expected = { 1, 2, 3, 4, 5 };
+        algorithm.QuickSort(testArray, 0, testArray.Length - 1);
+        CollectionAssert.AreEqual(expected, testArray,
+            "Is Quick Sort working as inteneded?");
+    }
+
+    [TestMethod]
+    public void Swap_Working_Correctly()
+    {
+        int expectedValueX = 5;
+        int expectedValueY = 8;
+
+        int x = 8;
+        int y = 5;
+
+        algorithm.Swap(ref x, ref y);
+
+        Assert.IsTrue(x == expectedValueX,
+            "Has the value X been switched with Y?");
+
+        Assert.IsTrue(y == expectedValueY,
+            "Has the value Y been switched with X?");
+    }
+
+
+    [TestMethod]
+    public void Partition_Working_Correctly()
+    {
+        int[] testArray = { 4, 2, 5, 1, 3 };
+        int low = 0;
+        int high = testArray.Length - 1;
+        int pivotIndex = algorithm.Partition(testArray, low, high);
+
+        int pivotValue = testArray[pivotIndex];
+        for (int i = 0; i < pivotIndex; i++)
+        {
+            Assert.IsTrue(testArray[i] <= pivotValue,
+                "Element on the left of pivot is greater than pivot.");
+        }
+
+        for (int i = pivotIndex + 1; i <= high; i++)
+        {
+            Assert.IsTrue(testArray[i] > pivotValue,
+                "Element on the right of pivot is less than or equal to pivot.");
+        }
+
+        Assert.IsTrue(pivotIndex >= low && pivotIndex <= high,
+            "Pivot index is outside the range of low and high.");
+    }
+}
---
+++ Project4Sorting/Project4Sorting/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.IO;
+using System.Reflection;
+
+namespace Project4
+{
+
+    public class SortingAlgorithm
+    {
+        public void Swap(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public int Partition(int[] data, int low, int high)
+        {
+            int pivot = data[high];
+            int i = low - 1;
+            for (int j = low; j < high; j++)
+            {
+                if (data[j] < pivot)
+                {
+                    i++;
+                    Swap(ref data[i], ref data[j]);
+                }
+            }
+            Swap(ref data[i + 1], ref data[high]);
+            return i + 1;
+        }
+
+        public void QuickSort(int[] data, int low, int high)
+        {
+            if (low < high)
+            {
+                int pi = Partition(data, low, high);
+                QuickSort(data, low, pi - 1);
+                QuickSort(data, pi + 1, high);
+            }
+        }
+
+        public void BubbleSort(int[] data)
+        {
+            int n = data.Length;
+            bool noSwap;
+
+            do
+            {
+                noSwap = true;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    if (data[i] > data[i + 1])
+                    {
+                        Swap(ref data[i], ref data[i + 1]);
+                        noSwap = false;
+                    }
+                }
+            }
+            while (!noSwap);
+        }
+
+        static void Main()
+        {
+            SortingAlgorithm algorithm = new SortingAlgorithm();
+            int[] sizes = { 500, 1_000, 2_000, 5_000, 10_000 };
+
+            string fileName = "sortTimes.csv";
+            string filePath = findCurrentPath(fileName);
+
+            if (!File.Exists(filePath))
+            {
+                File.WriteAllText(filePath, "Size,BubbleSortTime,QuickSortTime\n");
+            }
+
+            foreach (var size in sizes)
+            {
+                int[] bubbleSortData = Enumerable.Range(1, size).OrderBy(x => Guid.NewGuid()).ToArray();
+                int[] quickSortData = (int[])bubbleSortData.Clone();
+                Stopwatch sw = Stopwatch.StartNew();
+                algorithm.BubbleSort(bubbleSortData);
+                sw.Stop();
+
+                double bubbleSortTime = sw.Elapsed.TotalSeconds;
+                Console.WriteLine($"Bubble Sort Time: {bubbleSortTime} seconds");
+
+                sw.Restart();
+                algorithm.QuickSort(quickSortData, 0, quickSortData.Length - 1);
+                sw.Stop();
+
+                double quickSortTime = sw.Elapsed.TotalSeconds;
+                Console.WriteLine($"Quick Sort took: {quickSortTime} s\n");
+
+                File.AppendAllText(filePath, $"{size},{bubbleSortTime},{quickSortTime}\n");
+            }
+        }
+
+        static string findCurrentPath(string filename)
+        {
+
+            string exePath = Assembly.GetExecutingAssembly().Location;
+
+            DirectoryInfo exeDirectory = new DirectoryInfo(Path.GetDirectoryName(exePath));
+            DirectoryInfo targetDirectory = exeDirectory.Parent?.Parent?.Parent;
+
+            string filePath = Path.Combine(targetDirectory.FullName, filename);
+
+            return filePath;
+        }
+
+
+    }
+}
